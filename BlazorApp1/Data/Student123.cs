8087261e2019d0ef55012d66c82f8974e95db97b
Александр Краснов <krsasha200393@gmail.com>
2022-01-13 11:52:38 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp1/Data/Student123.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class Student123
+    {
+        public string name;
+        public string surname;
+        public int group;
+        public string faculty;
+
+        public Student123(string name, string surname, int group, string faculty)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.group = group;
+            this.faculty = faculty;
+        }
+    }
+}
