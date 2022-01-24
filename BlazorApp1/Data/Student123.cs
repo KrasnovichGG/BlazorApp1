b6b2b6419c805c4f3556d4631e9f3b8370987ded
Александр Краснов <krsasha200393@gmail.com>
2022-01-24 12:51:55 +0300

    Регистация в монго

--- BlazorApp1/Data/Student123.cs
+++ BlazorApp1/Data/Student123.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+
 using System.Threading.Tasks;
 
 namespace BlazorApp1.Data
 {
     public class Student123
     {
+        [BsonId][BsonIgnoreIfDefault]
+        public ObjectId id { get; set; }
+        [BsonElement]
         public string name;
+        [BsonElement]
         public string surname;
+        [BsonElement]
         public int group;
+        [BsonElement]
         public string faculty;
+        public static List<string> titles=new List<string>() { "Имя","Фамилия","Факультет","Группа"};
 
         public Student123(string name, string surname, int group, string faculty)
         {
@@ -18,6 +29,13 @@ namespace BlazorApp1.Data
             this.surname = surname;
             this.group = group;
             this.faculty = faculty;
+        }
+        public static List<Student123> GetStudents()
+        {
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("StudentBlazor(Napitok)");
+            var collection = db.GetCollection<Student123>("StudetsCollection");
+            return collection.Find(x => true).ToList();
         }
     }
 }
---
+++ BlazorApp1/Data/StudentExtansion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using MongoDB.Driver;
+namespace BlazorApp1.Data
+{
+    public class StudentExtansion
+    {
+        public static List<Student123> list = Student123.GetStudents();
+        public static List<string> titles = Student123.titles;
+
+        public async static void Add(Student123 student)
+        {
+            list.Add(student);
+            MongoClient client = new MongoClient();
+            var db = client.GetDatabase("StudentBlazor");
+            var collection = db.GetCollection<Student123>("StudentBlazorCollection");
+            await collection.InsertOneAsync(student);
+
+        }
+    }
+}
